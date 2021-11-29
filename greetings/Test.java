d544e6397f2566b0f741e34f7d82865ed58ff285
jrduncan1 <dunc5201@gmail.com>
2021-11-29 15:48:23 -0700

    init java commit

---
+++ greetings/Test.java
@@ -0,0 +1,15 @@
+// File name must be the same as the class name
+// Each file IS a class
+
+public class Test{
+    // The file or class with the exact method signature below
+    // is always the launching point of the application.
+    // You cannot run a Java program without it. Every Java program will
+    // have one and ONLY one class file that has a "main" method.
+    // Think of the "main" method as the launchpad for all your apps.
+
+    public static void main(String[] args){
+        // For now, all your code goes here.
+        System.out.println("Hello world!");
+    }
+}
